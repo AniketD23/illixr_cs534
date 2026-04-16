7cc512f0a83c4f0609bea9eb71cb4a0ae0d30e2d
AniketD23 <akdas3@illinois.edu>
2026-04-16 10:30:36 -0500

    starting to set up experiments

--- cs534_project/ada_baseline.sh
+++ cs534_project/ada_baseline.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+# loop through experiments specified in config directory
+
+    # run illixr server with yaml file of experiment i
+
+    # run script to get latency and number of faces from output
+
+    # stretch: run script for f-scores
